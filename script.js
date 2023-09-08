aca0d431f058f101ff9a38caf39c511b4fb6193f
GitCGDA <cgda@hotmail.co.uk>
2023-09-08 22:31:24 +0800

    my commit

--- script.js
+++ script.js
@@ -2,22 +2,21 @@ let slideIndex = 0;
 showSlide(slideIndex);
 
 function changeSlide(n) {
-    showSlide(slideIndex += n);
+    showSlide((slideIndex += n));
 }
 
 function showSlide(n) {
-    let slides = document.getElementsByClassName("slide");
-
+    const slides = document.querySelectorAll(".slide");
     if (n >= slides.length) {
         slideIndex = 0;
     }
     if (n < 0) {
         slideIndex = slides.length - 1;
     }
-
-    for (let i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-
-    slides[slideIndex].style.display = "block";
+    document.querySelector(".slides").style.transform = `translateX(-${slideIndex * 100}%)`;
 }
+
+// Automatic slide change every 3 seconds
+setInterval(() => {
+    changeSlide(1);
+}, 3000);
