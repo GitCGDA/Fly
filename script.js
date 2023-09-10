8ce2000d118c74a45149d168fa38a94142c33640
GitCGDA <cgda@hotmail.co.uk>
2023-09-10 13:03:15 +0800

    my commit

--- script.js
+++ script.js
@@ -1,29 +1,27 @@
-// JavaScript for the slider
-let slideIndex = 0;
-const slides = document.querySelectorAll('.slide');
+let currentSlide = 0;
+        const slides = document.querySelectorAll(".slide");
 
-function showSlide(n) {
-    if (n < 0) {
-        slideIndex = slides.length - 1;
-    } else if (n >= slides.length) {
-        slideIndex = 0;
-    }
-    for (let i = 0; i < slides.length; i++) {
-        slides[i].style.display = 'none';
-    }
-    slides[slideIndex].style.display = 'block';
-}
+        function showSlide(slideIndex) {
+            if (slideIndex < 0) {
+                currentSlide = slides.length - 1;
+            } else if (slideIndex >= slides.length) {
+                currentSlide = 0;
+            }
+            for (let i = 0; i < slides.length; i++) {
+                slides[i].style.display = "none";
+            }
+            slides[currentSlide].style.display = "block";
+        }
 
-function nextSlide() {
-    showSlide(slideIndex + 1);
-    slideIndex++;
-}
+        function prevSlide() {
+            showSlide(currentSlide - 1);
+        }
 
-function prevSlide() {
-    showSlide(slideIndex - 1);
-    slideIndex--;
-}
+        function nextSlide() {
+            showSlide(currentSlide + 1);
+        }
 
-// Automatically advance the slider
-setInterval(nextSlide, 5000); // Change slide every 5 seconds
-showSlide(slideIndex); // Show the initial slide
+        showSlide(currentSlide);
+    </script>
+</body>
+</html>
