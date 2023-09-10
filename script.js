3717e423f461a4a33f79e7742a5670689ce32dd0
GitCGDA <cgda@hotmail.co.uk>
2023-09-10 18:08:04 +0800

    my commit

--- script.js
+++ script.js
@@ -1,17 +1,23 @@
-// JavaScript for image slider
 let slideIndex = 0;
+showSlide(slideIndex);
 
-function showSlides() {
-    const slides = document.querySelectorAll(".slider img");
+function changeSlide(n) {
+    showSlide(slideIndex += n);
+}
+
+function showSlide(n) {
+    let slides = document.getElementsByClassName("slide");
+
+    if (n >= slides.length) {
+        slideIndex = 0;
+    }
+    if (n < 0) {
+        slideIndex = slides.length - 1;
+    }
+
     for (let i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
     }
-    slideIndex++;
-    if (slideIndex > slides.length) {
-        slideIndex = 1;
-    }
-    slides[slideIndex - 1].style.display = "block";
-    setTimeout(showSlides, 2000); // Change image every 2 seconds
-}
 
-showSlides();
+    slides[slideIndex].style.display = "block";
+}
