61f15a425c90b9ab874025b6e892e5947abd9a54
GitCGDA <cgda@hotmail.co.uk>
2023-10-15 21:25:18 +0800

    my commit

--- script.js
+++ script.js
@@ -14,31 +14,3 @@ document.querySelector('.menu-icon').addEventListener('click', function() {
 
 
 
-
-
-
-
-let slideIndex = 0;
-
-    function showSlides() {
-        const slides = document.querySelectorAll('.slide');
-        const indicators = document.querySelectorAll('.indicator');
-
-        for (let i = 0; i < slides.length; i++) {
-            slides[i].style.display = 'none';
-            indicators[i].classList.remove('active');
-        }
-
-        slideIndex++;
-
-        if (slideIndex > slides.length) {
-            slideIndex = 1;
-        }
-
-        slides[slideIndex - 1].style.display = 'flex';
-        indicators[slideIndex - 1].classList.add('active');
-
-        setTimeout(showSlides, 2000); // Change slide every 2 seconds
-    }
-
-    showSlides();
