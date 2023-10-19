ea7cee6152cf060810c20e6eec44dc76782efd29
GitCGDA <cgda@hotmail.co.uk>
2023-10-19 16:43:32 +0800

    my commit

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
-function toggleMenu() {
-    var mobileMenu = document.getElementById("mobileMenu");
-    if (mobileMenu.style.display === "block") {
-        mobileMenu.style.display = "none";
+// Toggle the mobile menu when the mobile icon is clicked
+const mobileIcon = document.querySelector('.mobile-icon');
+const mobileMenu = document.querySelector('.mobile-menu');
+
+mobileIcon.addEventListener('click', function () {
+    if (mobileMenu.style.display === 'block' || mobileMenu.style.display === '') {
+        mobileMenu.style.display = 'none';
     } else {
-        mobileMenu.style.display = "block";
+        mobileMenu.style.display = 'block';
     }
-}
+});
