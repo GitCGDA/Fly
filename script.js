782c8d9348b139fc0c7a837987a876f4ca416722
GitCGDA <cgda@hotmail.co.uk>
2023-10-21 18:12:15 +0800

    my commit

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 function toggleMenu() {
     var mobileMenu = document.getElementById("mobileMenu");
-    if (mobileMenu.style.display === "block") {
-        mobileMenu.style.display = "none";
+    if (mobileMenu.style.left === "0px") {
+        mobileMenu.style.left = "-100%";
     } else {
-        mobileMenu.style.display = "block";
+        mobileMenu.style.left = "0px";
     }
 }
